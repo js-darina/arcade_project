262df95aea969c16dfb55d85acc3a49e461ec70d
just_darina <darina555star@gmail.com>
2026-01-30 22:40:03 +0700

    main.py

--- main.py
+++ main.py
@@ -1,113 +1,128 @@
-﻿import arcade
-import random
-
-# константы
+import arcade
+from random import randint
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = 'Hyperclimb'
 
-PLAYER_SCALE = 0.8
-PLAYER_SPEED_X = 5
-PLAYER_SPEED_Y = 2
 
-GRAVITY = 0.4
+class MenuView(arcade.View):
+    """Главное меню"""
 
-COIN_SCALE = 0.5
-
-# класс игрока
-
-class Player(arcade.Sprite):
-    def __init__(self, x, y):
-        super().__init__('player.png', PLAYER_SCALE)
-        self.center_x = x
-        self.center_y = y
-
-        self.change_x = 0
-        self.change_y = PLAYER_SPEED_Y
-
-        self.coins = 0
-        self.buffs = []
-        self.debuffs = []
-
-    def update(self):
-        self.center_x += self.change_x
-        self.center_y += self.change_y
-
-        if self.left < 0:
-            self.left = 0
-        if self.right > SCREEN_WIDTH:
-            self.right = SCREEN_WIDTH
-
-# окно игры
-
-class HyperclimbGame(arcade.Window):
     def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-        arcade.set_background_color(arcade.color.DARK_SLATE_BLUE)
+        super().__init__()
+        self.background_color = arcade.color.WHITE
 
-        self.player = None
-        self.player_list = None
-        self.wall_list = None
-        self.coin_list = None
-
-        self.physics_engine = None
-
-    def setup(self):
-        self.player_list = arcade.SpriteList()
-        self.wall_list = arcade.SpriteList()
-        self.coin_list = arcade.SpriteList()
-
-        self.player = Player(SCREEN_WIDTH // 2, 100)
-        self.player_list.append(self.player)
-
-        for i in range(10):
-            coin = arcade.Sprite('coin.png', COIN_SCALE)
-            coin.center_x = random.randint(50, SCREEN_WIDTH - 50)
-            coin.center_y = random.randint(200, 2000)
-            self.coin_list.append(coin)
-
-        self.physics_engine = arcade.PhysicsEnginePlatformer(
-            self.player, self.wall_list, gravity_constant=GRAVITY
+        # Тексты пересчитываются каждый кадр в on_draw()
+        self.main_text = arcade.Text(
+            'HyperCar', 0, 0, arcade.color.BLACK,
+            font_size=40, anchor_x='center'
+        )
+        self.space_text = arcade.Text(
+            'Нажмите SPACE, чтобы начать!', 0, 0,
+            arcade.color.BLACK, font_size=20, anchor_x='center'
         )
 
     def on_draw(self):
-        arcade.start_render()
-        self.wall_list.draw()
-        self.coin_list.draw()
-        self.player_list.draw()
+        self.clear()
+        self.main_text.x = self.window.width / 2
+        self.main_text.y = self.window.height / 2 + 50
+        self.space_text.x = self.window.width / 2
+        self.space_text.y = self.window.height / 2 - 50
 
-        arcade.draw_text(
-            f'Coins: {self.player.coins}',
-            10, SCREEN_HEIGHT - 30,
-            arcade.color.WHITE, 16
-        )
-
-    def on_update(self, delta_time):
-        self.player_list.update()
-        self.physics_engine.update()
-
-        coins_hit = arcade.check_for_collision_with_list(self.player, self.coin_list)
-        for coin in coins_hit:
-            coin.remove_from_sprite_lists()
-            self.player.coins += 1
+        self.main_text.draw()
+        self.space_text.draw()
 
     def on_key_press(self, key, modifiers):
-        if key == arcade.key.A:
-            self.player.change_x = -PLAYER_SPEED_X
-        elif key == arcade.key.D:
-            self.player.change_x = PLAYER_SPEED_X
+        if key == arcade.key.SPACE:
+            game_view = GameView()
+            self.window.show_view(game_view)
+
+
+class GameView(arcade.View):
+
+    def __init__(self):
+        super().__init__()
+        self.background_color = arcade.color.WHITE
+
+        self.sprite_list = arcade.SpriteList()
+
+        # игровой персонаж
+        self.player = arcade.Sprite(None)
+
+        self.player.center_x = 100
+        self.player.center_y = 100
+        self.player.scale = 0.5
+
+        # препятствие
+        self.wall = arcade.Sprite(None)
+        self.wall.center_x = randint(1, 800)
+        self.wall.center_y = 600
+        self.wall.scale = 0.6
+
+        # добавляем спрайты в список
+        self.sprite_list.append(self.player)
+#       self.sprite_list.append(self.wall)
+
+        self.player_speed = 5
+        self.walls_speed = 3
+
+    def on_draw(self):
+        self.clear()
+        self.sprite_list.draw()
+        self.wall.change_y = -self.walls_speed
+
+    def on_update(self, delta_time):
+        self.sprite_list.update()
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.ESCAPE:
+            pause_view = PauseView(self)
+            self.window.show_view(pause_view)
+        # Движение персонажа
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player.change_x = -self.player_speed
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player.change_x = self.player_speed
 
     def on_key_release(self, key, modifiers):
-        if key == arcade.key.A or key == arcade.key.D:
+        if key in (arcade.key.LEFT, arcade.key.RIGHT, arcade.key.A, arcade.key.D):
             self.player.change_x = 0
 
 
-def main():
-    window = HyperclimbGame()
-    window.setup()
-    arcade.run()
+class PauseView(arcade.View):
+    def __init__(self, game_view):
+        super().__init__()
+        self.game_view = game_view
+        self.background_color = arcade.color.WHITE
+
+        self.pause_text = arcade.Text(
+            'ПАУЗА', 0, 0, arcade.color.BLACK,
+            font_size=40, anchor_x='center'
+        )
+        self.space_text = arcade.Text(
+            'SPACE - продолжить | ESC - в меню', 0, 0,
+            arcade.color.BLACK, font_size=20, anchor_x='center'
+        )
+
+    def on_draw(self):
+        self.clear()
+        self.pause_text.x = self.window.width / 2
+        self.pause_text.y = self.window.height / 2 + 25
+        self.space_text.x = self.window.width / 2
+        self.space_text.y = self.window.height / 2 - 25
+
+        self.pause_text.draw()
+        self.space_text.draw()
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.SPACE:
+            self.window.show_view(self.game_view)  # возврат в игру
+        elif key == arcade.key.ESCAPE:
+            self.window.show_view(MenuView())  # в главное меню
 
 
 if __name__ == '__main__':
-    main()
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, 'HyperCar')
+    menu_view = MenuView()
+    window.show_view(menu_view)
+    arcade.run()
